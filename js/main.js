73b41ea6054c76cb5231260431a4a01722cb375d
Seregy-Yudin-1981 <usv81@mail.ru>
2021-09-29 15:13:45 +0500

    задание 2 исправил ошибки

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
 function randomInteger(min, max) {
+  if (min<0 || max<0) {
+    return;
+  }
   if ( max <= min) {
-    return console.log('Максимальное значение должно быть больше минимального');
+    return; // console.log('Максимальное значение должно быть больше минимального');
   }
   // случайное число от min до (max+1)
-  let rand = min + Math.random() * (max + 1 - min);
+  const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
-
-console.log('Случайное число: ' + randomInteger(1, 10));
+//console.log('Случайное число: ' +
+randomInteger(1, 10);
 
 function randomIntegerRounding(min, max, rounding) {
-  if ( max <= min) {
-    return console.log('Максимальное значение должно быть больше минимального');
+  if (min<0 || max<0) {
+    return; //console.log('Значения не могут быть отрицательными');
   }
-  let rand = min + Math.random() * (max + 1 - min);
-  //округляем указывая количество знаков после запятой
-  return rand.toFixed(rounding);
+  if ( max <= min) {
+    return; //console.log('Максимальное значение должно быть больше минимального');
+  }
+  const randRounding = min + Math.random() * (max + 1 - min);
+  //округляем указывая количество знаков после запятой в переменной "rounding"
+  return randRounding.toFixed(rounding);
 }
-
-console.log('Случайное число: ' + randomIntegerRounding(10, 10, 3));
+//console.log('Случайное число: '+
+randomIntegerRounding(10,10,3);
