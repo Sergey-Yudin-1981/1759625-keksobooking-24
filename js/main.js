c0bbd9f96bfd2149aff576d5b768ab2ea0d52d5b
Seregy-Yudin-1981 <usv81@mail.ru>
2021-09-29 12:56:17 +0500

    задание 2 добавил условие

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 function randomInteger(min, max) {
+  if ( max <= min) {
+    return console.log('Максимальное значение должно быть больше минимального');
+  }
   // случайное число от min до (max+1)
   let rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
@@ -7,9 +10,12 @@ function randomInteger(min, max) {
 console.log('Случайное число: ' + randomInteger(1, 10));
 
 function randomInteger(min, max) {
+  if ( max <= min) {
+    return console.log('Максимальное значение должно быть больше минимального');
+  }
   let rand = min + Math.random() * (max + 1 - min);
   //округляем указывая количество знаков после запятой
   return rand.toFixed(3);
 }
 
-console.log('Случайное число: ' + randomInteger(1, 10));
+console.log('Случайное число: ' + randomInteger(10, 10));
