ef4b34e6fc551b543294cf1c9a77f94c928b5b7d
Seregy-Yudin-1981 <usv81@mail.ru>
2021-09-29 16:35:31 +0500

    задание 2 объеденил условия

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 function randomInteger(min, max) {
-  if (min<0 || max<0) {
+  if (min<0 || max<0 || max <= min) {
     return;
   }
-  if ( max <= min) {
-    return; // console.log('Максимальное значение должно быть больше минимального');
-  }
+
   // случайное число от min до (max+1)
   const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
@@ -13,11 +11,8 @@ function randomInteger(min, max) {
 randomInteger(1, 10);
 
 function randomIntegerRounding(min, max, rounding) {
-  if (min<0 || max<0) {
+  if (min<0 || max<0 || max <= min) {
     return; //console.log('Значения не могут быть отрицательными');
-  }
-  if ( max <= min) {
-    return; //console.log('Максимальное значение должно быть больше минимального');
   }
   const randRounding = min + Math.random() * (max + 1 - min);
   //округляем указывая количество знаков после запятой в переменной "rounding"
