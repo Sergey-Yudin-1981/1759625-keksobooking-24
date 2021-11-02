ce8a60b20e9c3557789b488d1cec3616c22f44d5
Seregy-Yudin-1981 <usv81@mail.ru>
2021-11-02 15:12:39 +0500

    задание 7 объекты и массивы 1-2-3

--- js/action-on-off.js
+++ js/action-on-off.js
@@ -8,6 +8,15 @@ function disableForm (classFormMap, block) {
         elementForm[i].disabled = true; //добавление атрибута disabled элементу
       }
     }
+  } else {
+    for (let j = 0; j < classFormMap.length; j ++) {
+      const objectForm = document.querySelector(classFormMap[j]);
+      objectForm.classList.remove('ad-form--disabled');
+      const elementForm = objectForm.childNodes; //находим все дочерние элементы
+      for( let i = 0; i < elementForm.length; i++ ){
+        elementForm[i].disabled = false; //добавление атрибута disabled элементу
+      }
+    }
   }
 }
 export {disableForm};
