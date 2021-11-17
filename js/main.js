40a5121c04465a24e61f2aecfbb0c37a213b3df6
Seregy-Yudin-1981 <usv81@mail.ru>
2021-11-18 00:10:08 +0500

    задание 12 перламутровые пуговицы 2

--- js/main.js
+++ js/main.js
@@ -1,34 +1,16 @@
-//import {createHotelNumber} from './data.js';
 import {fillHotelElement} from './markup.js';
 import {disableForm} from './action-on-off.js';
 import {activatePopup} from './popup.js';
-//import {loadMap} from './blueMarkerObject.js';
 import {fetchedData} from './fetchHotelsData.js';
 
-//const note = createHotelNumber(10);
-
-//console.log(wizards);
-//console.log(note[1].location.lat);
-
-//вывод данных по объявлению (попап на карте)
-// const cardItem = fillHotelElement(note[3], '#card');
-// document.getElementById('map-canvas').append(cardItem);
-
-//подключениме карты
 const COORDINATES_LAT = 35.68172;
 const COORDINATES_LNG = 139.75392;
 const map = L.map('map-canvas');
-//подключаем свой маркер
 const redIcon = L.icon({
   iconUrl: 'img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
-// const blueIcon = L.icon({
-//   iconUrl: 'img/pin.svg',
-//   iconSize: [40, 40],
-//   iconAnchor: [20, 40],
-// });
 
 async function getHotelsData() {
   try {
@@ -83,9 +65,7 @@ async function renderBaloons() {
   disableForm(['.map__filters'], false);
 }
 
-//получаем данные с сервера при загрузке карты
 map.on('load', () => {
-  //loadMap(map);
   renderBaloons();
 });
 
@@ -93,7 +73,7 @@ map.setView({
   lat: COORDINATES_LAT,
   lng: COORDINATES_LNG,
 }, 10);
-//подключаем картографический сервис (для отображения карты)
+
 L.tileLayer(
   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
   {
@@ -102,7 +82,6 @@ L.tileLayer(
 ).addTo(map);
 
 const coordinatesInput = document.getElementById('address');
-//добавляем маркер на карту
 const markerRed = L.marker(
   {
     lat: COORDINATES_LAT,
@@ -116,7 +95,6 @@ const markerRed = L.marker(
 markerRed.addTo(map);
 
 coordinatesInput.value = `${COORDINATES_LAT}, ${COORDINATES_LNG}`;
-//отображение координат при перетаскивании маркера
 markerRed.on('move', (evt) => {
   const coordinates = evt.target.getLatLng();
   const currentLat = coordinates.lat.toFixed(5);
@@ -124,26 +102,7 @@ markerRed.on('move', (evt) => {
   //Выводим координаты в поле ввода адреса
   coordinatesInput.value = `${currentLat}, ${currentLng}`;
 });
-//отображаем несколько меток синиго цвета
-// for (let i=0; i < note.length; i ++) {
-//   const lat = note[i].location.lat;
-//   const lng = note[i].location.lng;
-//   const markerBlue = L.marker(
-//     {
-//       lat,
-//       lng,
-//     },
-//     {
-//       icon: blueIcon,
-//     },
-//   );
-//   markerBlue
-//     .addTo(map)
-//     .bindPopup(fillHotelElement(note[i], '#card'));
-// }
 
-// фильтрация
-// стартовое состояние фильтрация
 const filterValues = {
   type: 'any',
   price: 'any',
@@ -216,14 +175,12 @@ function updateFilterValues () {
   }
 })();
 
-//активное-неактивное состояние ввода данных
 const data = ['.ad-form', '.map__filters'];
 disableForm(data, true);
 map.whenReady(() => {
   disableForm(['.ad-form'], false);
 });
 
-//Проверка ввода заголовка объявления
 const MIN_HEADING_LENGTH = 30;
 const MAX_HEADING_LENGTH = 100;
 const headingNameInput = document.getElementById('title');
@@ -242,7 +199,6 @@ headingNameInput.addEventListener('input', () => {
   headingNameInput.reportValidity();
 });
 
-//Проверка ввода цены
 const priceNameInput = document.getElementById('price');
 const typeNameInput = document.getElementById('type');
 
@@ -275,13 +231,6 @@ priceNameInput.addEventListener('keyup', (evt) => {
   evt.target.reportValidity();
 });
 
-//когда меняется количество комнат
-//->
-//выбираем все варианты колиства гостей
-//->
-//ставим disabled вариантам гостей, большим, чем количество комнат
-//->
-//если комнат 100 - вручную оставляем активным только вариант не для гостей
 
 const roomNumberSelect = document.getElementById('room_number');
 const guestsNumberSelect = document.getElementById('capacity');
@@ -305,7 +254,6 @@ function handleRoomsChange() {
 window.addEventListener('load', handleRoomsChange);
 roomNumberSelect.addEventListener('change', handleRoomsChange);
 
-//время заезда синхронизированно с временем выезда и наоборот
 const timeInRoom = document.getElementById('timein');
 const timeOutRoom = document.getElementById('timeout');
 const timeInRoomOptions = timeInRoom.querySelectorAll('option');
@@ -318,51 +266,6 @@ function arrivalTimeInOut(elementValue, roomOptions) {
 }
 timeInRoom.addEventListener('change', () => { arrivalTimeInOut(timeInRoom.value, timeOutRoomOptions);});
 timeOutRoom.addEventListener('change', () => { arrivalTimeInOut(timeOutRoom.value, timeInRoomOptions);});
-
-//проверка запроса на сервера
-
-
-//получаем данные с сервера
-//map.whenReady(() => {
-// map.on('load', () => {
-//   console.log('загрузилось');
-//   fetch('https://24.javascript.pages.academy/keksobooking/data',
-//     {
-//       method: 'GET',
-//       credentials: 'same-origin',
-//     },
-//   )
-//     .then((response) => {
-//       if (response.ok) {
-//         return response.json();
-//       }
-//       throw new Error('Сервер не отвечает, повторите попытку позднее.');
-//     })
-//     .then((dataServer) => {
-//       //отображаем синие метки на карте
-//       for (let i=0; i < dataServer.length; i ++) {
-//         const lat = dataServer[i].location.lat;
-//         const lng = dataServer[i].location.lng;
-//         const markerBlue = L.marker(
-//           {
-//             lat,
-//             lng,
-//           },
-//           {
-//             icon: blueIcon,
-//           },
-//         );
-//         markerBlue
-//           .addTo(map)
-//           .bindPopup(fillHotelElement(dataServer[i], '#card'));
-//       }
-//       disableForm(['.map__filters'], false);
-//     })
-//     .catch(() => {
-//       activatePopup('serverError');
-//       disableForm(['.map__filters'], true);
-//     });
-// });
 
 function resetForm() {
   document.querySelector('.ad-form').reset();
@@ -409,11 +312,9 @@ formNode.addEventListener('submit', (evt) => {
       activatePopup('error');
     });
 });
-//фильтрация по типу помещения
 const typeHouse = document.getElementById('housing-type');
 
 function typeHouseChange() {
-  //console.log(typeHouse.value);
 
 }
 
